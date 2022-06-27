023726c4a063f8c554b55e6bd03ad2b9a9bb2dd0
ShaneWilmes <shanewilmes@yahoo.com>
2022-06-26 20:20:08 -0600

    set up User.js in models

--- models/User.js
+++ models/User.js
@@ -1,2 +1,45 @@
-// Similar to Assignment.js in MP
+const { Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,9})$/
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }],
+
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }]
+
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+      }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
+
 
