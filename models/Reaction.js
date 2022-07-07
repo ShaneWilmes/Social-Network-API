76e344773a0536bb0b98df6da630771d1690e295
ShaneWilmes <shanewilmes@yahoo.com>
2022-07-06 18:55:56 -0600

    wrote out reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,45 @@
+const { Schema, Types } = require('mongoose');
+const dayjs = require('dayjs');
+var advancedFormat = require('dayjs/plugin/advancedFormat');
+dayjs.extend(advancedFormat);
+
+const reactionSchema = new Schema(
+
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId(),
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 300
+        },
+        username: {
+            type: String,
+            required: true,
+
+        },
+        createdAt: {
+            type: Date,
+            default: dayjs(),
+            get: (date) => dayjs(date).format('MMM Do YYYY [at] h:mm A')
+        }
+    },
+
+    {
+        toJSON: {
+            getters: true
+        },
+
+        id: false,
+    }
+
+);
+
+
+module.exports = reactionSchema;
+
+
+
+
