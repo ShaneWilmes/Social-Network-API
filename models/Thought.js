ae68981283ba8dff3268cccb618f6ac20685f304
ShaneWilmes <shanewilmes@yahoo.com>
2022-07-09 13:44:58 -0600

    package.json issues

--- models/Thought.js
+++ models/Thought.js
@@ -1,47 +1,45 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
-const dayjs = require('dayjs');
-var advancedFormat = require('dayjs/plugin/advancedFormat');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const dayjs = require("dayjs");
+var advancedFormat = require("dayjs/plugin/advancedFormat");
 dayjs.extend(advancedFormat);
 
-
+// Schema to create a course model
 const thoughtSchema = new Schema(
-
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxLength: 300
-        },
-        createdAt: {
-            type: Date,
-            default: dayjs(),
-            get: (date) => dayjs(date).format('MMM Do YYYY [at] h:mm A')
-        },
-        username: {
-            type: String,
-            require: true
-        },
-        reactions: [reactionSchema]
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      // minlength: 1,
+      maxLength: 280,
     },
-    {
-        toJSON: {
-            getters: true
-        },
-
-        id: false,
-    }
-
+    createdAt: {
+      type: Date,
+      default: dayjs(),
+      get: (date) => dayjs(date).format("MMM Do YYYY [at] h:mm A"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-
-userSchema.virtual('thoughtCount').get(function () {
-    return this.reactions.length
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-const User = model('thought', thoughtSchema);
+const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
+
 
 
 
